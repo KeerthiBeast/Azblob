9ac353826442ef11761cd57fa0a972abf5931d1a
keerthibeast <keerthivasan000071@gmail.com>
2024-09-08 14:11:08 +0530

    Changes in refreshing the list

--- app/src/main/java/com/example/azblob/CloudStorage.kt
+++ app/src/main/java/com/example/azblob/CloudStorage.kt
@@ -2,7 +2,6 @@ package com.example.azblob
 
 import android.annotation.SuppressLint
 import android.os.Build
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.annotation.RequiresApi
 import androidx.compose.animation.AnimatedVisibility
@@ -44,24 +43,17 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.azblob.model.Blob
 import com.example.azblob.utils.BlobViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import com.example.azblob.model.BlobFinal
-import com.example.azblob.utils.syncFolder
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -144,7 +136,7 @@ fun CloudStorage(viewModel: BlobViewModel = viewModel(), paddingValues: PaddingV
                     LaunchedEffect(true) {
                         scope.launch {
                             pullToRefreshState.startRefresh()
-                            delay(2500L)
+                            delay(2000L)
                             viewModel.getSongs()
                             pullToRefreshState.endRefresh()
                         }
@@ -177,7 +169,7 @@ fun CloudStorage(viewModel: BlobViewModel = viewModel(), paddingValues: PaddingV
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
 fun BlobItem(blob: BlobFinal, downloader: DownloaderImp) {
-    val name = blob.name?: "Unknown blob"
+    val name = blob.name
     val fname = if(name != "Unknown Blob") {
         name.substring(0, name.length-4)
     } else {
@@ -187,32 +179,6 @@ fun BlobItem(blob: BlobFinal, downloader: DownloaderImp) {
         modifier = Modifier
             .fillMaxWidth()
     ) {
-        /*if(listFiles?.contains(name) != true) {
-            Text(
-                text = fname,
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(16.dp),
-                overflow = TextOverflow.Ellipsis,
-                maxLines = 1
-            )
-            IconButton(onClick = { downloader.downloadFile(blob.url!!, name) }) {
-                Icon(
-                    painter = painterResource(R.drawable.cloud_download),
-                    contentDescription = null
-                )
-            }
-        } else {
-            Text(
-                text = fname,
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(16.dp),
-                color = Color.Green,
-                overflow = TextOverflow.Ellipsis,
-                maxLines = 1
-            )
-        }*/
         Text(
             text = fname,
             modifier = Modifier
